512b92082801525c2f0b050a5235b7f770b1ecba
Coding 4 Girls <coding4girls@via-internet.de>
2024-05-19 09:03:31 +0200

    -

--- bin/make-screenhots.sh
+++ bin/make-screenhots.sh
@@ -1,12 +1,21 @@
+#!/usr/local/bin/bash
+
+THIS="$0"
+[[ "$THIS" == "-bash" ]] && THIS="$1"
+
+BASE="$(readlink -f "$THIS")"
+BASE="$(dirname "$BASE")"
+
+echo BASE=$BASE
+
 find . -name *.html 				|\
 grep -v node_modules 				|\
 while read FILE
 do
 	SHOT=${FILE/.html/.png};
-	SHOT=${SHOT/solution/exercise};
-
+	# SHOT=${SHOT/solution/exercise};
 	echo "$FILE -> $SHOT"
 
-	node screenshot.js  $FILE $SHOT
+	node $BASE/screenshot.js  $FILE $SHOT
 done
 
