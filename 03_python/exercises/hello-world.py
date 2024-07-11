06c64fbe7e18d4ad3aedd229577109ac3497ba3b
Prashant Lamture <prashant.s.lamture@gmail.com>
2024-07-11 20:05:15 +0530

    print

--- 03_python/exercises/hello-world.py
+++ 03_python/exercises/hello-world.py
@@ -1,1 +1,4 @@
 print("Hello,dear uncle How are you?")
+print("Hello,dear Sis How are you?")
+print("Hello, uncle How are you?")
+print("Hello,Bro How are you?")
--- 03_python/pratice-python/first-program.py
+++ 03_python/pratice-python/first-program.py
@@ -1,1 +1,4 @@
 print("welcome to india please uncle, I am waiting for you")
+print("Hello,dear uncle How are you?")
+print("Hello,dear How are you?")
+print("Hello, How are you?")
